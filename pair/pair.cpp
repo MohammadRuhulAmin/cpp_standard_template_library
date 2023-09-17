53777b068e9aed3839bd6cfa91194cd087a0c8ff
ruhul <ra.01403288711@gmail.com>
2023-09-17 14:06:46 +0600

    basic pair decleration

---
+++ pair/pair.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	pair<int,string>p;
+	p = make_pair(1,"ruhul amin");
+	cout << p.first << " " << p.second << endl;
+	pair<int,string>x;
+	x = {1,"Mumtahina"};
+	cout << x.first << " " << x.second << endl;
+	return 0;
+}
